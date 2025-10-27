d8d58e9244cbdd225c38da0947c272c16fd854ab
Kabi Rengoku <kabi.rengoku2004@gmail.com>
2025-10-27 19:39:27 +0530

    Map Chip Filter First Commit

--- app/src/main/java/com/kabi/minichallenge/MainActivity.kt
+++ app/src/main/java/com/kabi/minichallenge/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContent
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.kabi.minichallenge.sept25DesignTheFest.DynamicUpdateScreen
-import com.kabi.minichallenge.sept25DesignTheFest.ExpandableListScreen
+import androidx.compose.ui.graphics.Color
+import com.kabi.minichallenge.sept25DesignTheFest.MapScreen
 import com.kabi.minichallenge.ui.theme.MiniChallengeTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,8 +28,15 @@ class MainActivity : ComponentActivity() {
                     /*ExpandableListScreen(
                         modifier = Modifier.padding(innerPadding)
                     )*/
-                    DynamicUpdateScreen(
+                    /*DynamicUpdateScreen(
                         modifier = Modifier.padding(innerPadding)
+                    )*/
+                    MapScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .background(
+                                color = Color(0xFFFFFFF0)
+                            )
                     )
                 }
             }
